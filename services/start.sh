1d3c642ee52ddc77c3dae6ea5035530e3f9fcad1
Brian Gebel <Brian Gebel>
2025-04-27 06:56:04 -0700

    move away from submodule

--- services/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker stack deploy -c ./traefik/docker-compose.yml proxy
-docker stack deploy -c ./portainer/docker-compose.yml portainer
-docker stack deploy -c ./librechat/docker-compose.yml librechat
-docker stack deploy -c ./stable-diffusion/docker-swarm.yml diffusion
